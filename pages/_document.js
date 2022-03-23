b03c995db3ca4600f181bcfcc446e8622c4805fa
Marko Kezic <marko.k96@web.de>
2022-03-23 23:55:30 +0100

    bg-pattern addded

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="de">
       <Head />
-      <body className="bg-gray-200 dark:bg-bgColor">
+      <body className="bg-gray-200 dark:bg-bgColor pattern-isometric pattern-neutral-800 pattern-bg-transparent pattern-opacity-100 pattern-size-8">
         <Main />
         <NextScript />
       </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,28 @@ module.exports = {
   ],
   theme: {
     extend: {
+      patterns: {
+        opacities: {
+          100: "1",
+          80: ".80",
+          60: ".60",
+          40: ".40",
+          20: ".20",
+          10: ".10",
+          5: ".05",
+        },
+        sizes: {
+          1: "0.25rem",
+          2: "0.5rem",
+          4: "1rem",
+          6: "1.5rem",
+          8: "2rem",
+          16: "4rem",
+          20: "5rem",
+          24: "6rem",
+          32: "8rem",
+        },
+      },
       screens: {
         xxs: "335px",
       },
@@ -30,5 +52,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-bg-patterns")],
 };
