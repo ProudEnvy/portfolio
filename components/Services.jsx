997af2d94997e843723333ddbb750b22f6fd0cf8
Marko Kezic <marko.k96@web.de>
2022-03-31 19:49:40 +0200

    webhosting and app deployment added

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Card from "./services/Card";
+import DeployCard from "./services/DeployCard";
 import flutterLogo from '../images/flutter.svg'
 import nextLogo from '../images/next-js.svg'
 import strapiLogo from '../images/strapi.svg'
+import appleLogo from '../images/apple.svg'
+import googleLogo from '../images/google.svg'
+import netlifyLogo from "../images/netlify-icon.svg";
+import microsoftLogo from "../images/microsoft-icon.svg";
+import linuxLogo from '../images/linux-icon.svg'
+
+
 
 const Services = () => {
   const mobileDevServiceText = new Map();
   const webDevServiceText = new Map();
   const backendServiceText = new Map();
+  const hostingServiceText = new Map();
+  const hostingAppText = new Map();
+
   mobileDevServiceText["title"] = "Mobile App Entwicklung";
   mobileDevServiceText["logo"] = flutterLogo;
   mobileDevServiceText["more"] = "https://flutter.dev/";
@@ -28,13 +39,41 @@ const Services = () => {
     "Mit Hilfe eines Headless Content Managment Systems, kann man Inhalte sehr leicht anpassen und verwalten. Die Daten werden dann durch eine API an die Applikation geschickt und verwertet";
   backendServiceText["popular"] = "";
 
+
+  hostingServiceText['title'] = 'Hosting Service';
+  hostingServiceText["more"] = "";
+  hostingServiceText["logo"] = [linuxLogo, microsoftLogo, netlifyLogo];
+  hostingServiceText["description"] = "Hochladen der Webseite auf einem Webserver, damit die Seite jederzeit im Internet erreichbar ist.";
+  hostingServiceText["popular"] = "";
+
+  hostingAppText['title'] = 'App Deployment';
+  hostingAppText['logo'] = [appleLogo, googleLogo];
+  hostingAppText["more"] = "";
+  hostingAppText["description"] = "Hochladen und Einrichtung der App Seite im Google Play und/oder Apple App Store";
+  hostingAppText["popular"] = "";
+
+
   return (
     <div id="services" className="flex flex-col items-center mt-32">
-      <div className="py-24 text-4xl underline underline-offset-2">Was ich anbiete</div>
-      <div className="flex flex-col transition-all duration-400 xl:gap-20 lg:flex-row lg:gap-8">
-        <Card data={mobileDevServiceText} />
-        <Card data={webDevServiceText} />
-        <Card data={backendServiceText} />
+      <div className="py-24 text-4xl underline underline-offset-2">
+        Was ich anbiete:
+      </div>
+      <div className="flex flex-col">
+        <div className="pb-4 text-2xl text-center">Softwarelösungen:</div>
+        <div className="flex flex-col transition-all duration-400 xl:gap-20 lg:flex-row lg:gap-8">
+          <Card data={mobileDevServiceText} />
+          <Card data={webDevServiceText} />
+          <Card data={backendServiceText} />
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <div className="pt-24 pb-4 text-2xl text-center">
+          Hosting der Anwendung/en:
+        </div>
+        <div className="flex flex-col transition-all duration-400 xl:gap-20 lg:flex-row lg:gap-8">
+          <DeployCard data={hostingServiceText} />
+          <DeployCard data={hostingAppText} />
+        </div>
       </div>
     </div>
   );
