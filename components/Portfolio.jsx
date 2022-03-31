d87023e81b129a349bb298dbc6dd084c5e613ca8
Marko Kezic <marko.k96@web.de>
2022-03-31 18:24:59 +0200

    project object added

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Project from './projects/project';
 
 const Portfolio = () => {
-  return <div id='portfolio' className="mt-32">Portfolio</div>;
+  return (
+    <div id="portfolio" className="mt-32">
+      <div>Portfolio</div>
+      <div>
+        <Project/>
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio
---
+++ components/projects/project.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const project = () => {
+  return (
+    <div>
+      project
+    </div>
+  );
+}
+
+export default project
